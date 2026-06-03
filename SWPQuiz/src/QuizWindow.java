7cc5125d6a1b23f11a93d1a75f438e7c04dd7e94
Lukas <hinterholzerlu@gmail.com>
2026-06-03 09:18:53 +0200

    StartFenster wurde hinzugefügt

---
+++ SWPQuiz/src/QuizWindow.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class QuizWindow extends JFrame {
+    QuizWindow() {
+        JFrame frame = new JFrame("Hauptframe");
+        frame.setSize(400,400);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+
+        frame.setVisible(true);
+    }
+
+}
