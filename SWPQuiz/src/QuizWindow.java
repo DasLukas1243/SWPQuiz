caf0c071ee2eea9549750fb4a00aa303ddbe3409
Lukas <hinterholzerlu@gmail.com>
2026-06-03 09:47:19 +0200

    Startbutton wurde hinzugefügt (ohne Funktion)

--- SWPQuiz/src/QuizWindow.java
+++ SWPQuiz/src/QuizWindow.java
@@ -1,10 +1,18 @@
 import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.BorderLayout;
 
 public class QuizWindow extends JFrame {
     QuizWindow() {
         JFrame frame = new JFrame("Hauptframe");
         frame.setSize(400,400);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
+
+
+        JButton startbutton = new JButton("Start");
+        frame.add(startbutton,BorderLayout.SOUTH);
+
 
 
 
